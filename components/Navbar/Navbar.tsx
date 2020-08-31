01d4750559a67455b05c2f2323588543da908574
andrealswa <swartz8@uwindsor.ca>
2020-08-31 15:39:49 -0400

    hamburger menu for mobile view

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -6,7 +6,11 @@ import Button from '@material-ui/core/Button';
 import styles from './Navbar.module.css';
 import { gql, useQuery } from '@apollo/client';
 import { client, signedInVar } from '../../apollo/client';
-
+import { IconButton } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import { useState } from 'react'
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
 
 const GET_SIGNED_IN = gql`
 query SignedIn {
@@ -30,80 +34,159 @@ export const Navbar = () => {
     return <div>loading...</div>;
   }
 
+  const [state, setState] = useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor: string, open: boolean) => (
+    event: React.KeyboardEvent | React.MouseEvent,
+  ) => {
+    if (
+      event.type === 'keydown' &&
+      ((event as React.KeyboardEvent).key === 'Tab' ||
+        (event as React.KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
   if (data)
     return (
       <div>
-        <AppBar position="static">
-          <div className={styles.toolbarContainer}>
-            <Toolbar className={styles.toolbar}>
-              <Link href="/">
-                <Typography className={styles.menuItemSpace} variant="h6">
-                  <a>Home</a>
-                </Typography>
-              </Link>
-
-              <Link href="/moviesearch">
-                <Typography variant="h6" className={styles.menuItemSpace}>
-                  <a>Movie Search</a>
-                </Typography>
-              </Link>
-
-              <Link href="/imagerepository">
-                <Typography variant="h6" className={styles.menuItemSpace}>
-                  <a>Image Repository</a>
-                </Typography>
-              </Link>
-
-              {data.signedInObject.signedInField && (
-                <Link href="/imageupload">
-                  <Typography variant="h6" className={styles.menuItemSpace}>
-                    <a>My Images</a>
+        <div className={styles.desktopAppBar}>
+          <AppBar position="static">
+            <div className={styles.toolbarContainer}>
+              <Toolbar className={styles.toolbar}>
+                <Link href="/">
+                  <Typography className={styles.menuItemSpace} variant="h6">
+                    <a>Home</a>
                   </Typography>
                 </Link>
+
+                <Link href="/moviesearch">
+                  <Typography variant="h6" className={styles.menuItemSpace}>
+                    <a>Movie Search</a>
+                  </Typography>
+                </Link>
+
+                <Link href="/imagerepository">
+                  <Typography variant="h6" className={styles.menuItemSpace}>
+                    <a>Image Repository</a>
+                  </Typography>
+                </Link>
+
+                {data.signedInObject.signedInField && (
+                  <Link href="/imageupload">
+                    <Typography variant="h6" className={styles.menuItemSpace}>
+                      <a>My Images</a>
+                    </Typography>
+                  </Link>
+                )}
+
+                <div className={styles.loginItem}>
+                  {!data.signedInObject.signedInField && (
+                    <span>
+                      <Link href="/login">
+                        <a>
+                          <Button variant="contained" color="primary">
+                            Log in
+                        </Button>
+                        </a>
+                      </Link>
+                    </span>
+                  )}
+                  {!data.signedInObject.signedInField && (
+                    <span className={styles.signup}>
+                      <Link href="/signup">
+                        <a>
+                          <Button color="inherit">Sign up</Button>
+                        </a>
+                      </Link>
+                    </span>
+                  )}
+
+                  {data.signedInObject.signedInField && (
+                    <span className={styles.displayEmail}>
+                      {data.signedInObject.email}
+                    </span>
+                  )}
+                  {data.signedInObject.signedInField && (
+                    <span>
+                      <Link href="/">
+                        <a>
+                          <Button onClick={handleLogout} variant="contained" color="primary">
+                            Log out
+                        </Button>
+                        </a>
+                      </Link>
+                    </span>
+                  )}
+                </div>
+              </Toolbar>
+            </div>
+          </AppBar>
+        </div>
+
+        <div className={styles.mobileAppBar}>
+          <div>
+            <Drawer anchor={'left'} open={state['left']} onClose={toggleDrawer('left', false)}>
+              <Button onClick={toggleDrawer('left', false)} >
+                <Link href="/">
+                  <Typography className={styles.menuItemSpace} variant="h6">
+
+                    <a className={styles.drawerButton}><HomeRoundedIcon />Home</a>
+                  </Typography>
+                </Link>
+              </Button>
+
+              <Button onClick={toggleDrawer('left', false)}>
+                <Link href="/moviesearch">
+                  <Typography variant="h6" className={styles.menuItemSpace}>
+                    <a>Movie Search</a>
+                  </Typography>
+                </Link>
+              </Button>
+
+              <Button onClick={toggleDrawer('left', false)}>
+                <Link href="/imagerepository">
+                  <Typography variant="h6" className={styles.menuItemSpace}>
+                    <a>Image Repository</a>
+                  </Typography>
+                </Link>
+              </Button>
+
+              {data.signedInObject.signedInField && (
+                <Button onClick={toggleDrawer('left', false)}>
+                  <Link href="/imageupload">
+                    <Typography variant="h6" className={styles.menuItemSpace}>
+                      <a>My Images</a>
+                    </Typography>
+                  </Link>
+                </Button>
               )}
-
-              <div className={styles.loginItem}>
-                {!data.signedInObject.signedInField && (
-                  <span>
-                    <Link href="/login">
-                      <a>
-                        <Button variant="contained" color="primary">
-                          Log in
-                        </Button>
-                      </a>
-                    </Link>
-                  </span>
-                )}
-                {!data.signedInObject.signedInField && (
-                  <span className={styles.signup}>
-                    <Link href="/signup">
-                      <a>
-                        <Button color="inherit">Sign up</Button>
-                      </a>
-                    </Link>
-                  </span>
-                )}
-
-                {data.signedInObject.signedInField && (
-                  <span className={styles.displayEmail}>
-                    {data.signedInObject.email}
-                  </span>
-                )}
-                {data.signedInObject.signedInField && (
-                  <span>
-                    <Link href="/">
-                      <a>
-                        <Button onClick={handleLogout} variant="contained" color="primary">
-                          Log out
-                        </Button>
-                      </a>
-                    </Link>
-                  </span>
-                )}
-              </div>
-            </Toolbar>
+            </Drawer>
           </div>
-        </AppBar>
+
+          <AppBar position="static">
+            <div className={styles.mobileToolbarContainer}>
+              <Toolbar>
+                <IconButton onClick={toggleDrawer('left', true)} edge="start" color="inherit" aria-label="menu">
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6">
+
+                  Home
+            </Typography>
+                <Button color="inherit">Login</Button>
+              </Toolbar>
+            </div>
+          </AppBar>
+        </div>
       </div>
     );
 };
