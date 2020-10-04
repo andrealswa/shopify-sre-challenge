9c317222d848409472e53c12e4045244f12a84fd
andrealswa <swartz8@uwindsor.ca>
2020-10-04 12:35:16 -0400

    filter based on movie

--- components/MovieSearchList/MovieSearchList.tsx
+++ components/MovieSearchList/MovieSearchList.tsx
@@ -1,6 +1,6 @@
 import { moviesListSearchedState } from '../../lib/recoil-atoms';
 import { useRecoilState } from 'recoil';
-import axios from "axios"
+import axios from 'axios';
 
 import styles from './MovieSearchList.module.css';
 import { MovieCardSearchResult } from '../MovieCardSearchResult/MovieCardSearchResult';
@@ -8,25 +8,36 @@ import { TextField } from '@material-ui/core';
 
 export const MovieSearchList = () => {
   const [moviesList, setMoviesList] = useRecoilState(moviesListSearchedState);
-  const url = new URL("https://www.omdbapi.com/?apikey=75b50ccc")
+  const url = new URL('https://www.omdbapi.com/?apikey=75b50ccc');
   // const CancelToken = axios.CancelToken;
   // let cancel
 
   const handleMovieSearch = async (event) => {
-    console.log(event.target.value.replace(" ", "_"))
-    let results
-    url.searchParams.set("s", event.target.value.replace(" ", "_"))
+    console.log(event.target.value.replace(' ', '_'));
+    let results;
+    url.searchParams.set('s', event.target.value.replace(' ', '_'));
     try {
-      results = await axios.get(url.href)
+      results = await axios.get(url.href);
     } catch (error) {
-      console.log("Request cancelled")
+      console.log('Request cancelled');
     }
-    console.log(results)
-    console.log(url)
+    console.log(results);
+    console.log(url);
     if (results.data.Search) {
-      setMoviesList(results.data.Search)
+      interface SearchItem {
+        Poster: String;
+        Title: String;
+        Type: String;
+        Year: String;
+        imdbID: String;
+      }
+      setMoviesList(
+        results.data.Search.filter((item: SearchItem) => {
+          return item.Type === 'movie';
+        })
+      );
     }
-  }
+  };
 
   return (
     <div>
@@ -37,13 +48,9 @@ export const MovieSearchList = () => {
           label="Movie Search"
           variant="outlined"
         />
-
       </div>
 
-
-      {moviesList.length > 0 && <h1>
-        Movie Search list
-        </h1>}
+      {moviesList.length > 0 && <h1>Movie Search list</h1>}
       <div className={styles.container}>
         {moviesList.map((movie, key) => {
           return <MovieCardSearchResult movie={movie} key={key} />;
